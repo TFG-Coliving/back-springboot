8140f48991438168b92dbee237fd8fcf86e03f7e
lorien <loriengarper@campus.monlau.com>
2023-05-05 20:34:18 +0200

    Arreglado eliminacion de las funciones de imagenes

--- src/main/java/com/example/backspringboot/service/PropertyService.java
+++ src/main/java/com/example/backspringboot/service/PropertyService.java
@@ -3,17 +3,21 @@ package com.example.backspringboot.service;
 import com.example.backspringboot.model.Property;
 import com.example.backspringboot.model.Rent_review;
 import com.example.backspringboot.repository.PropertyRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
 @Service
+@AllArgsConstructor
 public class PropertyService {
 
     @Autowired
     private PropertyRepository propertyRepo;
+    private final SimpMessagingTemplate messagingTemplate;
 
     public List<Property> listAllProperties() {
         return propertyRepo.findAll();
@@ -25,5 +29,7 @@ public class PropertyService {
 
     public void saveProperty(Property property) {
         propertyRepo.save(property);
+
+        messagingTemplate.convertAndSend("/topic/properties", property);
     }
 }
